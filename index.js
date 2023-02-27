ba86bca182826937c97dba3ef65cd5ed33896fc1
Yaswanth kumar <yaswa779@gmail.com>
2023-02-27 19:29:58 +0530

    files added

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Mysql DB Connection
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Yash123@",
+    database:"userdb"
+});
+
+
+// Routes
+app.get("/",(req,res)=>{
+    res.json("This is a Home Page");
+})
+
+app.get("/books",(req,res)=>{
+    const q = "SELECT * from books";
+    db.query(q,(err,data)=>{
+        if(err){
+            return res.json(err);
+        }else{
+            return res.json(data);
+        }
+    })
+});
+
+app.post("/books",(req,res)=>{
+    const q = "INSERT into books (`title`,`desc`,`cover`) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err)return res.json(err);
+        return res.json("Books has been Created Succesfully");
+    })
+})
+
+
+
+app.listen(8800,()=>{
+    console.log("Server Started");
+});
